4a847e278abe36a6fe2f8b58949f76d784c4bebf
Tuan <dangtuan21@gmail.com>
2022-05-25 15:01:43 -0400

    4th

---
+++ client/src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p className="text-center">
+        <a href="http://www.copetting.com" style={{color: 'white'}}>CoPetting Company</a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
